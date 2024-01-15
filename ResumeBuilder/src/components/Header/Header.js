f4e5e179ed8ad1d9e00dcf7cb9bc86dfda474ca5
abhi maurya <98451783+Abhishek11364@users.noreply.github.com>
2024-01-15 15:00:05 +0000

    Update Header.js and Header.module.css

--- ResumeBuilder/src/components/Header/Header.js
+++ ResumeBuilder/src/components/Header/Header.js
@@ -1,22 +1,22 @@
 import React from "react";
-
-import resumeSvg from "../../assets/resume.svg";
-
 import styles from "./Header.module.css";
 
 function Header() {
   return (
     <div className={styles.container}>
-      <div className={styles.left}>
-        <p className={styles.heading}>
-          A <span>Resume</span> that stands out!
-        </p>
-        <p className={styles.heading}>
-          Make your own resume. <span>It's free</span>
-        </p>
-      </div>
-      <div className={styles.right}>
-        <img src={resumeSvg} alt="Resume" />
+      <h1 className={styles.topHeading}>Resume Builder</h1>
+      <div className={styles.content}>
+        <div className={styles.left}>
+          <p className={styles.heading}>
+            A <span>Resume</span> that stands out!
+          </p>
+          <p className={styles.heading}>
+            Make your own resume. <span>It's free</span>
+          </p>
+        </div>
+        <div className={styles.right}>
+          <img src="https://i.postimg.cc/3rnC17fs/Resume.jpg" alt="Resume" />
+        </div>
       </div>
     </div>
   );
